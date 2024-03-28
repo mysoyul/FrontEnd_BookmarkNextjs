4626f883199017a3f6b1c4731f42377f00a67509
mysoyul <vega2k_@naver.com>
2024-03-28 17:41:31 +0900

    Bookmarks 수정

--- src/app/components/Bookmarks.tsx
+++ src/app/components/Bookmarks.tsx
@@ -5,13 +5,14 @@ import Pagination from './Pagination';
 
 type Props = {
     //bookmarks: BookmarkType[]
-    bookmarks: BookmarksResponse
+    bookmarks: BookmarksResponse,
+    query?: string
 }
 
-const Bookmarks: React.FC<Props> = ({bookmarks}) => {
+const Bookmarks: React.FC<Props> = ({bookmarks, query}) => {
     return (
         <div>
-            <Pagination bookmarks={bookmarks} />
+            <Pagination bookmarks={bookmarks} query={query} />
             {
             bookmarks.data.map(book => <Bookmark key={book.id} bookmark={book} />)
             }            
