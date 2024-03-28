b2b0f2a1df694497ad8ca48407de3d3ab20f79a6
mysoyul <vega2k_@naver.com>
2024-03-28 15:37:47 +0900

    page param add

--- src/lib/bookmarkApi.tsx
+++ src/lib/bookmarkApi.tsx
@@ -4,8 +4,11 @@ import { BookmarksResponse } from "@/types"
 
 const API_BASE_URL = 'http://localhost:8080'
 
-export async function getAllBookmarks(): Promise<BookmarksResponse> {
-    const res = await axios.get<BookmarksResponse>(`${API_BASE_URL}/api/bookmarks`)
+export async function getAllBookmarks(page: number): Promise<BookmarksResponse> {
+    const API_URL = `${API_BASE_URL}/api/bookmarks?page=${page}`
+    console.log(`url = ${API_URL}`)
+
+    const res = await axios.get<BookmarksResponse>(API_URL)
 
     return res.data
 }
