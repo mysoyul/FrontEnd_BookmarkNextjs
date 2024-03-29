11e03ec4b03476921f74a7757894444a48f0219b
mysoyul <vega2k_@naver.com>
2024-03-29 09:51:04 +0900

    saveBookmark 에서 env 호출

--- src/lib/bookmarkAdd.tsx
+++ src/lib/bookmarkAdd.tsx
@@ -1,8 +1,10 @@
 import axios from "axios"
 
-const API_BASE_URL = 'http://localhost:8080'
+const getApiUrl = () => {
+    return process.env.NEXT_PUBLIC_SERVER_API_BASE_URL || process.env.NEXT_PUBLIC_CLIENT_API_BASE_URL
+}
 
 export async function saveBookmark(bookmark:{title: string, url: string}) {
-    const res = await axios.post(`${API_BASE_URL}/api/bookmarks`, bookmark)
+    const res = await axios.post(`${getApiUrl()}/api/bookmarks`, bookmark)
     return res.data
 }
